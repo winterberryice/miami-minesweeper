5b6396d754b60378ac457e073bebf12a104d2520
“kacper.sledz” <casper.sledx@gmail.com>
2022-11-05 11:45:27 +0100

    head config

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+        <link
+          href="https://fonts.googleapis.com/css?family=Permanent+Marker&display=swap"
+          rel="stylesheet"
+        />
+        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="./favicon.ico" type="image/x-icon" />
+
+        <title>Miami minesweeper</title>
       </head>
       <body>{children}</body>
     </html>
