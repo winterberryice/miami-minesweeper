186493358cf3ea33aff0bb9b8dedc96aa5298095
“kacper.sledz” <casper.sledx@gmail.com>
2022-11-11 22:56:05 +0100

    click on proximity cell spec

--- app/logic/Game.spec.tsx
+++ app/logic/Game.spec.tsx
@@ -1,6 +1,7 @@
 import { act, renderHook } from "@testing-library/react";
 import { useGame, UseGameProps } from "./Game";
 import { BoardCreator_Size_4_Mines_2 } from "./TestUtils";
+import { Status } from "./types";
 
 describe("Game", () => {
   const testingProps_board_4x4: UseGameProps = {
@@ -15,13 +16,26 @@ describe("Game", () => {
     expect(result.current.board.flatMap((item) => item).length).toBe(16);
   });
 
-  test("should have board with mines", () => {
+  test(`click on cell with proximity mines 0 should open any neighboring cells,
+  if neighbor cell is proximity 0 then open recursively`, () => {
     const { result } = renderHook(() => useGame(testingProps_board_4x4));
 
     act(() => {
-      result.current.start();
+      result.current.onCellClick({ row: 0, column: 3 });
     });
 
-    //TODO expect
+    const openedCells = result.current.board
+      .flatMap((item) => item)
+      .filter((cell) => {
+        return (
+          cell.row === 2 ||
+          cell.row === 3 ||
+          cell.column === 2 ||
+          cell.column === 3
+        );
+      });
+
+    expect(openedCells.length).toBe(12);
+    expect(openedCells.every((cell) => cell.status === Status.OPEN)).toBe(true);
   });
 });
--- app/logic/types.tsx
+++ app/logic/types.tsx
@@ -1,3 +1,8 @@
+export type Coordinates = {
+  row: number;
+  column: number;
+};
+
 export enum Status {
   DEFAULT = "DEFAULT",
   OPEN = "OPEN",
